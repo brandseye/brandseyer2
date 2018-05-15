a846e3f0fc1651db6fbbaf4f92a8988510ff2b29
Constance Neeser <connie@brandseye.com>
2018-05-15 18:30:45 +0200

    Added a general method for reading from the API

---
+++ R/api_read_api.R
@@ -0,0 +1,41 @@
+# Copyright (c) 2018, Brandseye PTY (LTD)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#' Read arbitrary data from our api.
+#'
+#' This reads arbitrary data from our api, including mention
+#' data.
+#'
+#' @param endpoint The endpoint that you would like to access
+#' @param query An httr query list
+#'
+#' @return the json content
+#'
+#' @author Constance Neeser
+read_api <- function(endpoint, query = list()) {
+  assertthat::assert_that(assertthat::is.string(endpoint))
+  auth = whoami(raise_error = TRUE)
+
+  url = paste0("https://api.brandseye.com/", endpoint)
+  data <- httr::GET(url, httr::authenticate("API_KEY", auth$key), query = query)
+  check_errors(data)
+  return(httr::content(data))
+}

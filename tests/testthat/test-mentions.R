917badb0b9622df25a850df21a5676e15dafaea3
Constance Neeser <connie@brandseye.com>
2018-07-21 12:37:30 +0200

    Added many more tests

--- tests/testthat/test-mentions.R
+++ tests/testthat/test-mentions.R
@@ -5,6 +5,9 @@ test_that("Can parse a minimal list of mention data", {
   data <- list(
     list(id = "one",
          sentiment = as.integer(0),
+         published = "2017-11-21T21:37:35.000+0000",
+         pickedUp = "2017-11-21T21:37:35.000+0000",
+         updated = "2017-11-21T21:37:35.000+0000",
          brands = list(
            list(id = as.integer(1), name = "one"),
            list(id = as.integer(2), name = "two")
@@ -18,11 +21,16 @@ test_that("Can parse a minimal list of mention data", {
          socialNetwork = list(
            id = "TWITTER",
            label = "Twitter"
-         ))
+         )
+    ),
+    list(id = "two",
+         sentiment = as.integer(1),
+         tags = list()
+    )
   )
 
   m <- brandseyer2:::list_to_v4_mentions(data)
-  expect_equal(nrow(m), 1)
+  expect_equal(nrow(m), 2)
   expect_equal(m[[1, "id"]] , "one")
   expect_equal(m[[1, "brands"]], c(1, 2))
   expect_equal(m[[1, "sentiment"]], 0)
@@ -31,6 +39,10 @@ test_that("Can parse a minimal list of mention data", {
   expect_equal(m[[1, "mediaLinks"]]$url, "http://bob.com")
   expect_equal(m[[1, "mediaLinks"]]$mimeType, "application/json")
   expect_equal(m[[1, "socialNetwork"]], "TWITTER")
+
+  expect_equal(m[[2, "id"]], "two")
+  expect_equal(m[[2, "tags"]], NULL)
+  expect_equal(m[[2, "mediaLinks"]], tibble::tibble(url = NA, mimeType = NA))
 })
 
 test_that("Always needs a filter to get mentions", {
@@ -44,5 +56,10 @@ test_that("Can read mention data for an account", {
 
   expect_is(attr(m, "account"), "brandseyer2.account")
   expect_gt(nrow(m), 0)
+})
 
+test_that("Can handle reading no data", {
+  result <- brandseyer2:::list_to_v4_mentions(list())
+
+  expect_equal(result, tibble::tibble())
 })

b9301182937d9c712573aaac843e1477b628e133
Lara Basson <laraebasson@gmail.com>
2018-10-25 13:27:55 +0200

    Updated website

---
+++ R/account_account_client_code.R
@@ -0,0 +1,50 @@
+# Copyright (c) 2018, Brandseye PTY (LTD)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#' Get an account's client code
+#'
+#' Returns a vector of account client codes.
+#'
+#' @param account The account object, or list of account objects.
+#'
+#' @return A string holding the account's client code.
+#' @export
+#'
+#' @examples
+#'
+#' account("TEST01AA") %>%
+#'   account_client_code()
+#'
+#' account("TEST01AA", "TEST02AA") %>%
+#'   account_client_code()
+account_client_code <- function(account) {
+  UseMethod("account_client_code")
+}
+
+#' @export
+account_client_code.brandseyer2.account <- function(account) {
+  account$clientCode %||% NA
+}
+
+#' @export
+account_client_code.list <- function(account) {
+  map_chr(account, account_client_code)
+}
